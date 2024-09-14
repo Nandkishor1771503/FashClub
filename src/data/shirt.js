fb1b1f92c412f22abf3df1b0fa2a3f87799fba68
Nandkishor1771503 <160572418+Nandkishor1771503@users.noreply.github.com>
2024-09-14 16:55:18 +0530

    Shirts commited

--- src/data/shirt.js
+++ src/data/shirt.js
@@ -1,10 +1,15 @@
+import shirt1 from '../images/Shirts1.jpg'
+import shirt2 from '../images/Shirts2.jpg'
+import shirt3 from '../images/Shirts3.jpg'
+import shirt4 from '../images/Shirts4.jpg'
+
 let Shirts_data = [
 
     {
       id: 1,
       item: 'Bulls T-shirt',
       price: '₹200-₹500',
-      source : "https://i.etsystatic.com/19154517/r/il/8f8dfe/2870883939/il_fullxfull.2870883939_e1g7.jpg",
+      source : shirt1,
       discription : "Show your team spirit with our Chicago Bulls T-shirt, perfect for every fan of the Windy City’s basketball powerhouse! This design features a bold, classic Chicago Bulls logo prominently displayed on the chest, bringing together the team’s iconic red, black, and white color scheme."
     },
 
@@ -12,7 +17,7 @@ let Shirts_data = [
       id: 2,
       item: 'Lakers hoddie',
       price: '$50.00',
-      source : "https://tse3.mm.bing.net/th?id=OIP.DpqA2MUsQO3novn9NU83yQHaGB&pid=Api&P=0&h=180",
+      source : shirt2,
       discription : "Elevate your game-day attire with our Los Angeles Lakers Black Hoodie, designed for true fans of the iconic NBA team. This hoodie features a sleek, all-black design with a striking Lakers logo printed prominently on the front, showcasing your allegiance in style."
     },
 
@@ -20,7 +25,7 @@ let Shirts_data = [
       id: 3,
       item: 'Celtics white',
       price: '$15.99',
-      source : "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/51a0e458-e615-4e3d-aef3-ce35793b44f7/boston-celtics-dri-fit-nba-practice-t-shirt-NclGGQ.png",
+      source : shirt3,
       discription : "Celebrate the rich legacy of the Boston Celtics with our sleek and stylish white T-shirt, a must-have for any devoted fan. Featuring a crisp, clean design, this shirt showcases the iconic Celtics logo front and center, highlighted by the team’s classic green and white color scheme."
     },
 
@@ -28,7 +33,7 @@ let Shirts_data = [
       id: 4,
       item: 'Harden red ',
       price: '$10.00',
-      source : "https://mir-s3-cdn-cf.behance.net/project_modules/disp/5ed94651761671.58f8e2ca8a3c8.jpg",
+      source : shirt4,
       discription : "Showcase your support for one of the game’s greatest with our James Harden Red T-Shirt. This vibrant, eye-catching shirt features a dynamic design celebrating the iconic playmaker’s impact on the court. The front boasts a bold graphic of James Harden’s name and number, rendered in striking detail against a rich red backdrop."
     }
 
