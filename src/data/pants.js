87976a9ba7e2ad0d4f6a1ddd48af427295f4b588
Nandkishor1771503 <160572418+Nandkishor1771503@users.noreply.github.com>
2024-09-14 17:19:52 +0530

    images commited

--- src/data/pants.js
+++ src/data/pants.js
@@ -1,31 +1,36 @@
+import pant1 from '../images/Pants1.jpg'
+import pant2 from '../images/Pants2.jpg'
+import pant3 from '../images/Pants3.jpg'
+import pant4 from '../images/Pants4.jpg'
+
 let Pants_data = [
 
     {
       id: 1,
-      item: 'Lakers: Logo Mash Shorts',
+      item: 'Lakers:  Mash Shorts',
       price: '$10.99',
-      source: "https://m.media-amazon.com/images/I/71GLaRpCdML._SY879_.jpg",
+      source: pant1,
       discription : "Gear up in style with our Los Angeles Lakers Logo Mash Shorts, designed for fans who want to show their team spirit while enjoying maximum comfort. These shorts feature a vibrant, all-over print that combines multiple Lakers logos and team colors, creating a dynamic, eye-catching look that stands out whether you’re on the court or relaxing at home."
     },
     {
       id: 2,
       item: 'Chicago bulls jersey',
       price: '$$19.99',
-      source: "https://m.media-amazon.com/images/I/61W7haB616L._SX679_.jpg",
+      source: pant2,
       discription : "Elevate your game-day attire with our Chicago Basketball Jersey, a must-have for every fan of the Windy City’s basketball legacy. This jersey features a classic design with the iconic Chicago Bulls logo prominently displayed on the front, capturing the essence of one of the NBA’s most storied teams."
     },
     {
       id: 3,
-      item: 'heat pink women jersey',
+      item: 'Miami women jersey',
       price: '$18.00',
-      source: "https://m.media-amazon.com/images/I/41SbwY0dHYL.jpg",
+      source: pant3,
       discription : "Turn heads and show off your Miami Heat pride with our vibrant Pink Women’s Jersey, a stylish and standout addition to any fan’s wardrobe. This jersey features a sleek pink design, bringing a fresh and fashionable twist to the classic Miami Heat look."
     },
     {
       id: 4,
       item: 'Spaldingbasketball 7 ',
       price: '$25.00',
-      source: "https://m.media-amazon.com/images/I/41MupVR83eL._SX300_SY300_QL70_FMwebp_.jpg",
+      source: pant4,
       discription : "Elevate your game with the Spalding Highlight 7 Basketball, designed for players who demand top performance and reliability. This high-quality basketball features a durable composite leather cover that provides a superior grip and control, ensuring you can handle every dribble, pass, and shot with confidence."
     }
   ]
--- src/data/shoes.js
+++ src/data/shoes.js
@@ -1,30 +1,35 @@
+import shoes1 from '../images/Shoes1.jpg'
+import shoes2 from '../images/Shoes2.jpg'
+import shoes3 from '../images/Shoes3.jpg'
+import shoes4 from '../images/Shoes4.jpg'
+
 let Shoes_data = [
     {
       id: 1,
       item: 'Air Jordan 1 retro high',
       price: '$173.00',
-      source: "https://tse3.mm.bing.net/th?id=OIP.4ZfWEjq9qO925k4nDb-JewHaFM&pid=Api&P=0&h=180",
+      source: shoes1,
       discription : "Step into a piece of basketball history with the Air Jordan 1 Retro High, an iconic silhouette that continues to make a statement on and off the court. Originally released in 1985, this legendary sneaker combines timeless style with modern comfort, embodying the spirit and innovation of Michael Jordan's first signature shoe."
     },
     {
       id: 2,
       item: 'Nike ja 1',
       price: '$144.00',
-      source: "https://m.media-amazon.com/images/I/61XDYa1igqL._SX695_.jpg",
+      source: shoes2,
       discription: "Experience the pinnacle of performance and style with the Nike JA 1, designed for athletes who demand excellence on the court. Named after the dynamic player Ja Morant, this shoe blends cutting-edge technology with sleek design to deliver an exceptional basketball experience."
     },
     {
       id: 3,
       item: 'Nike  Lebron Witness',
       price: '$71.99',
-      source: "https://m.media-amazon.com/images/I/81vRwm45CZL._SX695_.jpg",
+      source: shoes3,
       discription: "Unleash your full potential on the court with the Nike Men’s LeBron Witness, a performance-driven basketball shoe inspired by the athletic prowess of LeBron James. Designed for players who demand both style and functionality, this shoe offers a perfect blend of comfort, support, and durability."
     },
     {
       id: 4,
       item: 'Currys Under Armour ',
       price: '$120.00',
-      source: "https://hypebeast.com/image/2015/01/under-armour-curry-one-0.jpeg",
+      source: shoes4,
       discription: "Elevate your game with the Under Armour Curry Basketball Shoes, designed for those who aspire to play with the precision and skill of NBA superstar Stephen Curry. Engineered for high performance, these shoes combine innovative technology with sleek design to offer unparalleled support, comfort, and style."
     }
 
